5242bbc2aa7dd383640a0c79e864061c841c404c
suraiyasultana826 <suraiyasultana826@gmail.com>
2023-11-18 17:57:23 +0600

    refetch

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ async function run() {
 
     const menuCollection = client.db('bistroDb').collection('menu');
     const reviewCollection = client.db('bistroDb').collection('reviews');
+    const cartCollection = client.db('bistroDb').collection('carts');
     app.get('/menu', async(req, res) => {
         const result = await menuCollection.find().toArray();
         res.send(result)
@@ -37,6 +38,19 @@ async function run() {
     app.get('/reviews', async(req, res) => {
         const result = await menuCollection.find().toArray();
         res.send(result)
+    })
+
+    //carts collection
+    app.get('/carts', async(req, res) => {
+      const email = req.query.email;
+      const query = {email:email};
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.post('/carts', async(req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
     })
 
 
